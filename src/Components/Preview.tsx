aa40e62c8d143f3a91c4ead01651068fcde182fc
Acquite <acquitefx@gmail.com>
2022-07-14 07:50:12 +0100

    a

---
+++ src/Components/Preview.tsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import '../CSS/Preview.css'
+
+const App: React.FC = () => {
+  return (
+    
+    <>
+      <input type='text' placeholder='State' className='stateInput'></input>
+      <input type='text' placeholder='Details' className='detailsInput'></input>
+    </>
+  )
+}
+
+export default App;
