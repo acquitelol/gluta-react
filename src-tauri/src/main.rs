a2c34d661f34bbb2ecbe85d49c70b094277b6975
Acquite <acquitefx@gmail.com>
2022-07-14 18:16:04 +0100

    persaeque

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,60 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent};
+use tauri::Manager;
+
 fn main() {
+  
+    
   tauri::Builder::default()
+    .on_system_tray_event(|app, event| match event {
+      SystemTrayEvent::LeftClick {
+        position: _,
+        size: _,
+        ..
+      } => {
+        let window = app.get_window("main").unwrap();
+        window.show().unwrap();
+      }
+      SystemTrayEvent::RightClick {
+        position: _,
+        size: _,
+        ..
+      } => {
+        let quit = CustomMenuItem::new("quit".to_string(), "╸Quit╺");
+        let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+        
+        let tray_menu = SystemTrayMenu::new()
+          .add_item(quit)
+          .add_native_item(SystemTrayMenuItem::Separator)
+          .add_item(hide);
+        let tray = SystemTray::new()
+          .with_menu(tray_menu);
+
+          tauri::Builder::default().system_tray(tray);
+      }
+      SystemTrayEvent::DoubleClick {
+        position: _,
+        size: _,
+        ..
+      } => {
+        println!("system tray received a double click");
+      }
+      SystemTrayEvent::MenuItemClick { id, .. } => {
+        match id.as_str() {
+          "quit" => {
+            std::process::exit(0);
+          }
+          "hide" => {
+            let window = app.get_window("main").unwrap();
+            window.hide().unwrap();
+          }
+          _ => {}
+        }
+      }
+      _ => {}
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/Components/Preview.tsx
+++ src/Components/Preview.tsx
@@ -3,7 +3,6 @@ import '../CSS/Preview.css'
 
 const App: React.FC = () => {
   return (
-    
     <>
       <input type='text' placeholder='State' className='stateInput'></input>
       <input type='text' placeholder='Details' className='detailsInput'></input>
